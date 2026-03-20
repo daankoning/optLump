ffb8d1207a3061696a38d8732de7db119ef3e12e
Daan Koning <daanolivierkoning@gmail.com>
2026-03-20 15:57:29 +0100

    Add nominal lumping functionality

--- R/lump.R
+++ R/lump.R
@@ -1,3 +1,5 @@
+#TODO: document
+
 #' Perform lumping on a hierarchical nominal variable
 #'
 #' @param data Factor or character vector of the categorical data
@@ -24,6 +26,41 @@ lump_hierarchical <- function(data, threshold, clusters, verbose = FALSE) {
   counts <- table(data)
 
   res <- maximum_mutual_information_hierarchical(counts, threshold, clusters, verbose = verbose)
+
+  lumpings <- res$lumping
+  names(lumpings) <- sapply(lumpings, \(x) paste(x, collapse = "+"))
+
+  levels(data) <- lumpings #TODO: replace with forcats
+
+  data
+}
+
+# TODO: this example can randomly break, so it should be tweaked
+#' Perform lumping on a nominal variable
+#'
+#' @param data Factor or character vector of the categorical data
+#' @param threshold The minimum number of samples each lumped level should contain
+#' @param adj_matrix Adjancency matrix of the preference graph
+#' @param verbose Logical value dictating if values should be printed. Default: `FALSE`
+#'
+#' @returns A factor vector with the lumped levels.
+#'
+#' @examples
+#' m <- 10
+#' n <- 100
+#' data <- sample(LETTERS[1:m], n, replace = TRUE)
+#' # Use a complete graph, so all lumpings are possible.
+#' adj <- matrix(1, nrow = m, ncol = m, dimnames = list(LETTERS[1:m], LETTERS[1:m]))
+#' lump_nominal(data, 15, adj)
+#'
+#' @author Daan Koning
+#' @export
+lump_nominal <- function(data, threshold, adj_matrix, verbose = FALSE) {
+  #TODO: validate inputs
+  data <- factor(data)
+  counts <- table(data)
+
+  res <- maximum_mutual_information_nominal(counts, threshold, adj_matrix, verbose = verbose)
 
   lumpings <- res$lumping
   names(lumpings) <- sapply(lumpings, \(x) paste(x, collapse = "+"))
