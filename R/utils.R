aac1561c1ac5130d1fdfe6282a50d6e2b179df5d
Daan Koning <daanolivierkoning@gmail.com>
2026-03-26 13:44:15 +0100

    Implement `adjacency_from_edge_list`

--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,26 @@ lumping_equal <- function(A, B) {
   setequal(lapply(A, sort), lapply(B, sort))
 }
 
+#TODO: document
+adjacancy_from_edge_list <- function(levels, allow = NULL, disallow = NULL) {
+  # TODO: validate inputs
+  if (!xor(is.null(allow), is.null(disallow))) {
+    stop("Exactly one of `allow` or `disallow` must be specified")
+  }
+  m <- length(levels)
+
+  default_symb <- if (!is.null(allow)) 0 else 1
+  positive_symb <- 1 - default_symb
+  edge_list <- if (!is.null(allow)) allow else disallow
+
+  adj <- matrix(default_symb, nrow = m, ncol = m, dimnames = list(levels, levels))
+
+  for (edge in edge_list) {
+    adj[edge[1], edge[2]] <- positive_symb
+    adj[edge[2], edge[1]] <- positive_symb
+  }
+
+  adj
+}
+
+#TODO: test
