5c40be17bda6e6767e726c2fc6a7649617fc0eaf
Daan Koning <daanolivierkoning@gmail.com>
2026-07-26 16:06:44 +0200

    test appliers

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -93,3 +93,85 @@ test_that("threshold_diagnostic validates argument combinations", {
     "heuristic"
   )
 })
+
+test_that("apply_nominal_lumping applies a lumping", {
+  data <- c("NL", "DE", "FR", "NL", "BE")
+  lumping <- list(benelux = c("NL", "BE"), other = c("DE", "FR"))
+
+  lumped <- apply_nominal_lumping(data, lumping)
+
+  expect_equal(as.character(lumped), c("benelux", "other", "other", "benelux", "benelux"))
+  expect_equal(levels(lumped), c("benelux", "other"))
+  expect_false(is.ordered(lumped))
+})
+
+test_that("apply_ordinal_lumping applies a lumping in the given order", {
+  data <- c("low", "high", "medium", "low", "very high")
+  lumping <- list("low" = "low", "medium+" = c("medium", "high", "very high"))
+
+  lumped <- apply_ordinal_lumping(data, lumping)
+
+  expect_true(is.ordered(lumped))
+  expect_equal(levels(lumped), c("low", "medium+"))
+  expect_equal(as.character(lumped), c("low", "medium+", "medium+", "low", "medium+"))
+})
+
+test_that("the appliers name unnamed lumpings with the level namer", {
+  data <- c("A", "B", "C")
+  lumping <- list(c("A", "B"), "C")
+
+  expect_equal(levels(apply_nominal_lumping(data, lumping)), c("A+B", "C"))
+  expect_equal(
+    levels(apply_ordinal_lumping(data, lumping, level_namer = \(x) paste(x, collapse = "-"))),
+    c("A-B", "C")
+  )
+})
+
+test_that("the appliers handle levels missing from the lumping or the data", {
+  lumping <- list(ab = c("A", "B"), cd = c("C", "D"))
+
+  # Levels in the lumping but not in the data are kept as empty levels
+  lumped <- apply_nominal_lumping(c("A", "B", "C"), lumping)
+  expect_equal(levels(lumped), c("ab", "cd"))
+  expect_equal(as.character(lumped), c("ab", "ab", "cd"))
+
+  # Levels in the data but not in the lumping become NA, with a warning
+  expect_warning(lumped <- apply_nominal_lumping(c("A", "X", "C"), lumping), "not in the lumping")
+  expect_equal(as.character(lumped), c("ab", NA, "cd"))
+
+  expect_warning(lumped <- apply_ordinal_lumping(c("A", "X"), lumping), "not in the lumping")
+  expect_equal(as.character(lumped), c("ab", NA))
+})
+
+test_that("the appliers validate the lumping", {
+  data <- c("A", "B")
+
+  expect_error(apply_nominal_lumping(data, "A"), "list of character vectors")
+  expect_error(apply_nominal_lumping(data, list()), "list of character vectors")
+  expect_error(apply_nominal_lumping(data, list(a = "A", "B")), "must be named")
+  expect_error(apply_ordinal_lumping(data, list(a = "A", a = "B")), "must be unique")
+  expect_error(apply_nominal_lumping(data, list(a = c("A", "B"), b = "A")), "exactly once")
+})
+
+test_that("apply_ordinal_lumping respects the level order of ordered data", {
+  data <- ordered(c("low", "medium", "high"), levels = c("low", "medium", "high"))
+
+  expect_error(
+    apply_ordinal_lumping(data, list(a = c("low", "high"), b = "medium")),
+    "level order"
+  )
+
+  lumped <- apply_ordinal_lumping(data, list(bottom = c("low", "medium"), top = "high"))
+  expect_equal(levels(lumped), c("bottom", "top"))
+})
+
+test_that("applying an optimizer's lumping matches the lump_ functions", {
+  set.seed(5)
+  data <- sample(LETTERS[1:5], 60, replace = TRUE)
+
+  res <- maximum_mutual_information_nominal(table(data), 15)
+  expect_equal(
+    as.character(apply_nominal_lumping(data, res$lumping)),
+    as.character(lump_nominal(data, 15))
+  )
+})
